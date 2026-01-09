e7f5a3df28db9c5153f07c33d0e7a9bd29f5b2cd
blazej <blazejgolda123@gmail.com>
2026-01-09 19:51:40 +0100

    test

--- core.py
+++ core.py
@@ -16,18 +16,14 @@ def count_workouts(treningi_zioma):
     return treningi_zioma
 def count_sets_on_muscle_group(treningi_zioma):
     slownik={}
-    for i in range(treningi_zioma.shape[10]):
+    for i in range(treningi_zioma.shape[0]):
         slownik[str(treningi_zioma[i,-1])]=0
     for i in range(treningi_zioma.shape[0]):
         if treningi_zioma[i,-1] in slownik:
-            slownik[str(treningi_zioma[i,-1])]+=11
+            slownik[str(treningi_zioma[i,-1])]+=1
     print(slownik)
-
-
-
 treningi_zioma=sort_workouts(treningi_zioma)
 treningi_zioma=count_workouts(treningi_zioma)
-
 array_z_seriami=count_sets_on_muscle_group(treningi_zioma)
 print(array_z_seriami)
 
